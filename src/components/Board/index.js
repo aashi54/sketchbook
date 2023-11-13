e1fa447c160dc2ffadaec5be81d678324bb4cda6
aashi54 <asmitapatidar2023@gmail.com>
2023-11-13 10:04:46 -0500

    toolbox color active status

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,11 @@
 import { useEffect, useRef } from "react";
+import {useSelector, useDispatch} from 'react-redux'
 
 const Board = () => {
     const canvasRef = useRef(null)
-
-    useEffect(() => {
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+    useEffect(() => { 
         if (!canvasRef.current) return
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d')
