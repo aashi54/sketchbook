590c80cecf67d61f32b95817586a75a0a1eb255a
aashi54 <asmitapatidar2023@gmail.com>
2023-11-14 03:16:33 -0500

    Provide the endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,6 @@
 import {io} from "socket.io-client";
 
-export const socket = io('http://localhost:5000');
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketchbook-server-zqzj.onrender.com' : 'http://localhost:5000'
+
+export const socket = io(URL);
 
